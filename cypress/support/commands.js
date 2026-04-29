5d4a4b091dc32202535d065c69029a71c811c0c5
ViraK0 <vira@atakama.com>
2026-04-29 11:48:40 +0300

    add registration tests and custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,30 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.overwrite("type", (originalFn, element, text, options) => {
+  if (options && options.sensitive) {
+    options.log = false;
+
+    Cypress.log({
+      $el: element,
+      name: "type",
+      message: "*".repeat(text.length),
+    });
+  }
+
+  return originalFn(element, text, options);
+});
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.contains("button", "Sign In").click();
+
+  cy.get("app-signin-modal").within(() => {
+    cy.get("#signinEmail").type(email);
+    cy.get("#signinPassword").type(password, { sensitive: true });
+
+    cy.contains("button", "Login").click();
+  });
+
+  cy.url().should("include", "/panel/garage");
+  cy.get("#userNavDropdown").should("be.visible").and("contain.text", "My profile");
+});
